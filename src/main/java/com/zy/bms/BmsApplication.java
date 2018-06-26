2a1eecd42712e81f7051b1f5df3f358aed831000
zhangyu <zhangyu@95190.com>
2018-06-26 11:00:10 +0800

    Initial commit

---
+++ src/main/java/com/zy/bms/BmsApplication.java
@@ -0,0 +1,12 @@
+package com.zy.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmsApplication.class, args);
+    }
+}
